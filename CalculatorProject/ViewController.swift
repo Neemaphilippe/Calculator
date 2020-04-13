97b579a326b38300d19c2d9720cedc2150067510
Neema Philippe <neemaphilippe@pursuit.org>
2020-04-13 10:51:39 -0400

    Added new variable to account for previous number and called it previousDisplayNumber, made it into a double, and started to set up the basic operations based on the sender tag

--- CalculatorProject/ViewController.swift
+++ CalculatorProject/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var displayNumber: Double = 0;
+    var previousDisplayNumber: Double = 0;
     
     @IBOutlet weak var displayLabel: UILabel!
     
@@ -24,6 +25,33 @@ class ViewController: UIViewController {
     
     
     @IBAction func nonNumberButtons(_ sender: UIButton) {
+        if displayLabel.text != nil && sender.tag != 11 && sender.tag != 16 {
+            
+            previousDisplayNumber = Double(displayLabel.text!)!
+            
+            //Operations:
+            if sender.tag == 12 {
+                displayLabel.text = "/"
+            }
+            if sender.tag == 13 {
+                displayLabel.text = "X"
+            }
+            if sender.tag == 14 {
+                displayLabel.text = "-"
+            }
+            if sender.tag == 15 {
+                displayLabel.text = "+"
+            }
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
         
     }
     
